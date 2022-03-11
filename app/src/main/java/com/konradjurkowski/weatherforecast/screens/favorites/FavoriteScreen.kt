3f77117248462c58273d69fab80ad9fd963960e9
Konrad <56559882+konradjurkowski@users.noreply.github.com>
2022-03-11 16:50:16 +0100

    WFA-3 setting up drop down menu (#2)
    WFA-3 setting up drop down menu (#2)

---
+++ app/src/main/java/com/konradjurkowski/weatherforecast/screens/favorites/FavoriteScreen.kt
@@ -0,0 +1,12 @@
+package com.konradjurkowski.weatherforecast.screens.favorites
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun FavoriteScreen(
+    navController: NavController
+) {
+    Text(text = "Favorite Screen")
+}
--- app/src/main/java/com/konradjurkowski/weatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/konradjurkowski/weatherforecast/widgets/WeatherAppBar.kt
@@ -1,22 +1,43 @@
 package com.konradjurkowski.weatherforecast.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.konradjurkowski.weatherforecast.R
 
 @Composable
 fun WeatherAppBar(
@@ -25,8 +46,14 @@ fun WeatherAppBar(
     isMainScreen: Boolean = true,
     elevation: Dp = 0.dp,
     onAddActionClicked: () -> Unit = { },
-    onButtonClicked: () -> Unit = { }
+    onButtonClicked: () -> Unit = { },
+    onMenuItemClicked: (DropDownMenuItems) -> Unit = { }
 ) {
+    val showDialog = remember { mutableStateOf(false) }
+    ShowSettingDropDownMenu(
+        showDialog = showDialog,
+        onMenuItemClicked = onMenuItemClicked
+    )
     TopAppBar(
         title = {
             Text(
@@ -46,7 +73,7 @@ fun WeatherAppBar(
                         contentDescription = "search_icon"
                     )
                 }
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { showDialog.value = true }) {
                     Icon(
                         imageVector = Icons.Rounded.MoreVert,
                         contentDescription = "menu_icon"
@@ -72,4 +99,56 @@ fun WeatherAppBar(
         backgroundColor = Color.Transparent,
         elevation = elevation
     )
+}
+
+@Composable
+fun ShowSettingDropDownMenu(
+    showDialog: MutableState<Boolean>,
+    onMenuItemClicked: (DropDownMenuItems) -> Unit = { }
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(
+                top = 45.dp,
+                right = dimensionResource(id = R.dimen.extra_large_padding)
+            )
+    ) {
+        DropdownMenu(
+            expanded = showDialog.value,
+            onDismissRequest = { showDialog.value = false },
+            modifier = Modifier
+                .width(150.dp)
+                .background(Color.White)
+        ) {
+            DropDownMenuItems.values().toList().forEach { item ->
+                DropdownMenuItem(
+                    onClick = {
+                        showDialog.value = false
+                        onMenuItemClicked(item)
+                    }) {
+                    Icon(
+                        imageVector = when (item) {
+                            DropDownMenuItems.ABOUT -> Icons.Default.Info
+                            DropDownMenuItems.FAVORITES -> Icons.Default.FavoriteBorder
+                            DropDownMenuItems.SETTINGS -> Icons.Default.Settings
+                        },
+                        contentDescription = null,
+                        tint = MaterialTheme.colors.primary
+                    )
+                    Spacer(modifier = Modifier.padding(dimensionResource(id = R.dimen.small_padding)))
+                    Text(text = stringResource(id = item.titleRes))
+                }
+            }
+        }
+    }
+}
+
+enum class DropDownMenuItems(
+    val titleRes: Int
+) {
+    ABOUT(R.string.about_item_label),
+    FAVORITES(R.string.favorites_item_label),
+    SETTINGS(R.string.settings_item_label)
 }
